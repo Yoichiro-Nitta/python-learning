8e5f46409d1713ee99d7655e4f9178707d3dc780
Yoichiro-Nitta <rayroad@me.com>
2024-02-23 21:50:40 +0000

    settings.py修正

--- python_learning/python_learning/settings.py
+++ python_learning/python_learning/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .settings_local import *
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
